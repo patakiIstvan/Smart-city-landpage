4440c0a72ed755c91586f25e07c74877119413be
Pataki István <patakiistvan1997y@gmail.com>
2022-03-02 01:44:38 +0100

    navbar test

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <header className="section">
+      <img src="" alt="" />
+      <nav>
+        <ul>
+          <li><a href="https://smartcity.netlify.app"></a></li>
+        </ul>
+      </nav>
+      <img src="" alt="" />
+    </header>
   )
 }
 
