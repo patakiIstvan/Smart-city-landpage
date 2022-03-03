39f237b201e75e88dcf998174c6327651027f02d
Pataki István <patakiistvan1997y@gmail.com>
2022-03-03 02:38:25 +0100

    footer styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,29 +1,32 @@
 import React from 'react'
+import facebookLogo from '../../assets/svg/facebook-logo.svg';
+import linkedinLogo from '../../assets/svg/linkedin-logo.svg';
+import twitterLogo from '../../assets/svg/twitter-logo.svg';
 
 function Footer() {
   return (
     <footer className='footer section'>
       <div className="container">
         <div className="left">
-          <h3>Mobile App Design & Development Company</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <h3 className='title'>Mobile App Design & Development Company</h3>
+          <p className="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
           <div className="icons">
-            <img className="icon" src="" alt="" />
-            <img className="icon" src="" alt="" />
-            <img className="icon" src="" alt="" />
+            <img className="icon fb" src={facebookLogo} alt="" />
+            <img className="icon in" src={linkedinLogo} alt="" />
+            <img className="icon twitter" src={twitterLogo} alt="" />
           </div>
         </div>
         <div className="right"></div>
-        <h3>Contact form</h3>
+        <h3 className='title title2'>Contact form</h3>
         <form action="">
-          <input type="text" name="fullName" id="fullName" />
-          <input type="email" name="" id="" />
-          <textarea name="txtmsg" id="txtmsg" cols="30" rows="10"></textarea>
-          <label htmlFor="txtmsg">maximium 200 karakter - 200 maradt</label>
-          <button>SEND <img src="" alt="" /></button>
+          <input className='input-field' type="text" name="fullName" id="fullName" />
+          <input className='input-field' type="email" name="" id="" />
+          <textarea className='input-field long-input' name="txtmsg" id="txtmsg" cols="30" rows="10"></textarea>
+          <label className='char-left' htmlFor="txtmsg">maximium 200 karakter - 200 maradt</label>
+          <button id='send'>SEND <img src="" alt="" /></button>
         </form>
       </div>
-      <p className="credit">© 2013, All rights reserved. Ponte.hu, H-1134 Budapest, Váci út 19. - Panoráma Ház 3.em.,  Tel.: (+36 1) 373 0733,  E-mail: info@ponte.hu  </p>
+      <p className="credit">© 2013, All rights reserved. <a href='https://ponte.hu'>Ponte.hu</a>, H-1134 Budapest, Váci út 19. - Panoráma Ház 3.em.,  Tel.: <a href='tel:3613730733'>(+36 1) 373 0733</a>,  E-mail: <a href='mailto:info@ponte.hu'>info@ponte.hu</a>  </p>
     </footer>
   )
 }
--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import cssLogo from '../../assets/svg/css-logo.svg';
+import htmlLogo from '../../assets/svg/html-logo.svg';
+import javaLogo from '../../assets/svg/java-logo.svg';
 
 function Partners() {
   return (
     <section className='partners section'>
+      <h2 className="title">Our partners</h2>
       <div className="container">
         <div className="container__item">
-          <img src="" alt="" />
+          <img className="css-logo" src={cssLogo} alt="logo of CSS3" />
         </div>
         <div className="container__item">
-          <img src="" alt="" />
+          <img className="html-logo" src={htmlLogo} alt="logo of HTML5" />
         </div>
         <div className="container__item">
-          <img src="" alt="" />
+          <img className="java-logo" src={javaLogo} alt="logo of java" />
         </div>
       </div>
     </section>
