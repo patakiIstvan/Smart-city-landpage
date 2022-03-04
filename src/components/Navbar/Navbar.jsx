249ed64bc9b9750a72cd9fcbee6868d41e9cfde9
Pataki István <patakiistvan1997y@gmail.com>
2022-03-04 05:18:09 +0100

    Better mobile experience + navbar essentials

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../../assets/svg/top-logo.svg'
+import menuIcon from '../../assets/svg/menu-icon.svg'
 
 function Navbar() {
+  // usestates
+  const [menuActive, setMenuAvtive] = useState(false);
+
+  function toggleMenu() {
+    setMenuAvtive(!menuActive);
+  }
+
   return (
     <header className="section navbar">
-      <img className="logo" src={logo} alt="" />
-      <nav>
-        <ul>
+      <img className="logo" src={logo} alt="logo" />
+      <nav className={menuActive && 'navbar-active'}>
+        <ul className='link-container'>
           <li><a href="https://verysmartcity.netlify.app">about</a></li>
           <li><a href="https://verysmartcity.netlify.app">services</a>
-            <ul>
+            <ul className='sublink-container'>
               <li><a href="https://verysmartcity.netlify.app">cms</a></li>
               <li><a href="https://verysmartcity.netlify.app">portal</a></li>
             </ul>
@@ -18,7 +26,9 @@ function Navbar() {
           <li><a href="https://verysmartcity.netlify.app">jobs</a></li>
         </ul>
       </nav>
-      <img src="" alt="" />
+      <div className={`menu-icon__container ${menuActive && 'menu-active'}`}>
+        <img className="menu-icon" src={menuIcon} alt="menu icon" onClick={toggleMenu} />
+      </div>
     </header>
   )
 }
