1037bb9aaf2abe87ec297c2cb8d59f690831d7ff
Pataki István <patakiistvan1997y@gmail.com>
2022-03-04 04:11:28 +0100

    working slidebar 😎

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,42 @@
 import React from 'react'
 import hero1 from '../../assets/image/hero1.png'
+import hero2 from '../../assets/image/hero2.jpg'
+import hero3 from '../../assets/image/hero3.jpg'
 
 function Hero() {
   return (
-    <section style={{ backgroundImage: `url(${hero1})` }} className="section hero">
-      <h1 className='title'>smart city</h1>
-      <p className='desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. <a href="https://verysmartcity.netlify.app">Read more</a></p>
+    <section className="hero">
+      <div style={{ backgroundImage: `url(${hero1})` }} className="slider1 slider section">
+        <h1 className='title'>smart city</h1>
+        <p className='desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. <br /><a href="https://verysmartcity.netlify.app">Read more</a></p>
 
-      <div className="page__container">
-        <div className="page__button active"></div>
-        <div className="page__button"></div>
-        <div className="page__button"></div>
+        <div className="page__container">
+          <div className="page__button active"></div>
+          <div className="page__button"></div>
+          <div className="page__button"></div>
+        </div>
       </div>
-    </section>
+      <div style={{ backgroundImage: `url(${hero2})` }} className="slider2 slider section">
+        <h1 className='title'>smart developers</h1>
+        <p className='desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. <br /><a href="https://verysmartcity.netlify.app">Read more</a></p>
+
+        <div className="page__container">
+          <div className="page__button"></div>
+          <div className="page__button active"></div>
+          <div className="page__button"></div>
+        </div>
+      </div >
+      <div style={{ backgroundImage: `url(${hero3})` }} className="slider3 slider section">
+        <h1 className='title'>truthly inspiring</h1>
+        <p className='desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. <br /><a href="https://verysmartcity.netlify.app">Read more</a></p>
+
+        <div className="page__container">
+          <div className="page__button "></div>
+          <div className="page__button"></div>
+          <div className="page__button active"></div>
+        </div>
+      </div >
+    </section >
   )
 }
 
