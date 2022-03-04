f9224e05a91c85a83d3b6a79c65a4b8bdb630daf
Pataki István <patakiistvan1997y@gmail.com>
2022-03-04 09:16:54 +0100

    fixed logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import logo from '../../assets/svg/top-logo.svg'
+import logo from '../../assets/svg/top-logo2.svg'
 import menuIcon from '../../assets/svg/menu-icon.svg'
 
 function Navbar() {
@@ -11,23 +11,35 @@ function Navbar() {
   }
 
   return (
-    <header className="section navbar">
-      <img className="logo" src={logo} alt="logo" />
-      <nav className={menuActive && 'navbar-active'}>
+    <header className="navbar">
+      <div className="container section">
+        <img className="logo" src={logo} alt="logo" />
+        <nav className='large-screen-nav'>
+          <ul className='link-container'>
+            <li><a href="https://verysmartcity.netlify.app">about</a></li>
+            <li className='sub-choose'><a href="https://verysmartcity.netlify.app">services</a>
+              <ul className='ls-sublink-container'>
+                <li><a href="https://verysmartcity.netlify.app">cms</a></li>
+                <li><a href="https://verysmartcity.netlify.app">portal</a></li>
+              </ul>
+            </li>
+            <li><a href="https://verysmartcity.netlify.app">works</a></li>
+            <li><a href="https://verysmartcity.netlify.app">jobs</a></li>
+          </ul>
+        </nav>
+        <div className={`menu-icon__container ${menuActive && 'menu-active'}`}>
+          <img className="menu-icon" src={menuIcon} alt="menu icon" onClick={toggleMenu} />
+        </div>
+      </div>
+      <div className={`mobile-menu ${menuActive && 'menu-links-active'}`}>
         <ul className='link-container'>
-          <li><a href="https://verysmartcity.netlify.app">about</a></li>
-          <li><a href="https://verysmartcity.netlify.app">services</a>
-            <ul className='sublink-container'>
-              <li><a href="https://verysmartcity.netlify.app">cms</a></li>
-              <li><a href="https://verysmartcity.netlify.app">portal</a></li>
-            </ul>
-          </li>
-          <li><a href="https://verysmartcity.netlify.app">works</a></li>
-          <li><a href="https://verysmartcity.netlify.app">jobs</a></li>
+          <li className="section"><a href="https://verysmartcity.netlify.app">about</a></li>
+          <li className="section"><a href="https://verysmartcity.netlify.app">services</a></li>
+          <li className='mobile-sublink section'><a href="https://verysmartcity.netlify.app">cms</a></li>
+          <li className='mobile-sublink section'><a href="https://verysmartcity.netlify.app">portal</a></li>
+          <li className="section"><a href="https://verysmartcity.netlify.app">works</a></li>
+          <li className="section"><a href="https://verysmartcity.netlify.app">jobs</a></li>
         </ul>
-      </nav>
-      <div className={`menu-icon__container ${menuActive && 'menu-active'}`}>
-        <img className="menu-icon" src={menuIcon} alt="menu icon" onClick={toggleMenu} />
       </div>
     </header>
   )
